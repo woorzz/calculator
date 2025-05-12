bc2b54b75d19c58d3f12fc0a5c29a0c457e3c954
Marine Langrez <115779031+woorzz@users.noreply.github.com>
2025-05-12 14:06:52 +0200

    fix: resolve ESLint parsing errors

--- app.vue
+++ app.vue
@@ -2,7 +2,8 @@
   <div>
     <NuxtRouteAnnouncer />
     <div
-      class="min-h-screen bg-gradient-to-tr from-gray-100 via-slate-200 to-gray-300 dark:from-gray-900 dark:via-gray-800 dark:to-gray-700 flex items-center justify-center">
+      class="min-h-screen bg-gradient-to-tr from-gray-100 via-slate-200 to-gray-300 dark:from-gray-900 dark:via-gray-800 dark:to-gray-700 flex items-center justify-center"
+    >
       <NuxtPage />
     </div>
   </div>
