c2e9f190b7ca302c981b6530584c1045eb50b2ce
Marine Langrez <115779031+woorzz@users.noreply.github.com>
2025-05-12 09:52:39 +0200

    feat(core): implement add() method

--- tests/unit/useCalculator.test.ts
+++ tests/unit/useCalculator.test.ts
@@ -1,8 +1,6 @@
 import { describe, it, expect } from 'vitest';
+import { add } from '~/composables/useCalculator';
 
-function add(a: number, b: number): number {
-    return a + b
-}
 describe('add', () => {
     it('should return the sum of two positive numbers', () => {
         expect(add(2, 3)).toBe(5);
